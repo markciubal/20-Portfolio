84081ee4486a9425e02c94e5aad4bf6b12c9f834
Mark Ciubal <mark.ciubal@gmail.com>
2023-08-10 18:48:17 -0700

    Working on web.js.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/js/bootstrap.bundle';
 
 function Header() {
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary rounded-3" aria-label="Eleventh navbar example">
+        <nav className="navbar navbar-expand-lg bg-body-tertiary" aria-label="Eleventh navbar example">
             <div className="container-md">
                 <a className="navbar-brand" href="#">MARK CIUBAL</a>
                 <button className="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbars" aria-controls="navbars" aria-expanded="false" aria-label="Toggle navigation">
@@ -15,25 +15,25 @@ function Header() {
                     <ul className="navbar-nav ms-auto">
                     {/* About Me, Portfolio, Contact, and Resume */}
                     <li className="nav-item ms-auto">
-                            <a className="btn rounded-2 btn-primary" href="#about">About Me</a>
+                            <a className="btn rounded-2" href="#about">About</a>
                         </li>
                         <li className="nav-item ms-auto">
-                            <a className="btn rounded-2 btn-primary" href="#art">Art</a>
+                            <a className="btn rounded-2" href="#art">Art</a>
                         </li>
                         <li className="nav-item ms-auto">
-                            <a className="btn rounded-2 btn-primary" href="#web">Web</a>
+                            <a className="btn rounded-2" href="#web">Web</a>
                         </li>
                         <li className="nav-item ms-auto">
-                            <a className="btn rounded-2 btn-primary" href="#photography">Photography</a>
+                            <a className="btn rounded-2" href="#photography">Photography</a>
                         </li>
                         <li className="nav-item ms-auto">
-                            <a className="btn rounded-2 btn-primary" href="#resume">Resume</a>
+                            <a className="btn rounded-2" href="#resume">Resume</a>
                         </li>
                         <li className="nav-item ms-auto">
-                            <a className="btn rounded-2 btn-primary" href="#footer">Contact</a>
+                            <a className="btn rounded-2" href="#footer">Contact</a>
                         </li>
                         {/* <li className="nav-item dropdown">
-                            <a className="btn rounded-2 btn-primary dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
+                            <a className="btn rounded-2 dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
                             <ul className="dropdown-menu">
                                 <li><a className="dropdown-item" href="#">Action</a></li>
                                 <li><a className="dropdown-item" href="#">Another action</a></li>
